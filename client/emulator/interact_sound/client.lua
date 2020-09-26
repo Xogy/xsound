43ed2b04ee5c61a07cf434b63274dacc92042967
MrXogos <49158267+Xogy@users.noreply.github.com>
2020-09-26 22:21:57 +0200

    Validated event 'InteractSound_CL:PlayWithinDistance'

--- client/emulator/interact_sound/client.lua
+++ client/emulator/interact_sound/client.lua
@@ -12,9 +12,11 @@ if config.interact_sound_enable then
 
     RegisterNetEvent('InteractSound_CL:PlayWithinDistance')
     AddEventHandler('InteractSound_CL:PlayWithinDistance', function(playerNetId, maxDistance, soundFile, soundVolume)
-        local eCoords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(playerNetId)))
-        PlayUrlPos("./sounds/" .. soundFile, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume, eCoords)
-        Distance("./sounds/" .. soundFile, maxDistance)
+        if GetPlayerFromServerId(playerNetId) ~= -1 then
+            local eCoords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(playerNetId)))
+            PlayUrlPos("./sounds/" .. soundFile, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume, eCoords)
+            Distance("./sounds/" .. soundFile, maxDistance)
+        end
     end)
 
 end
