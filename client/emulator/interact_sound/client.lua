9712aa81d8ed48ca60ebc57f8ff866d367af749b
Mohammad amin Jafarian <amin.jafarian82@gmail.com>
2021-05-28 15:41:59 +0430

    Support OneSync infinty

--- client/emulator/interact_sound/client.lua
+++ client/emulator/interact_sound/client.lua
@@ -2,23 +2,24 @@ if config.interact_sound_enable then
 
     RegisterNetEvent('InteractSound_CL:PlayOnOne')
     AddEventHandler('InteractSound_CL:PlayOnOne', function(soundFile, soundVolume)
-		math.randomseed(GetGameTimer())
-		local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
+        math.randomseed(GetGameTimer())
+        local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
         PlayUrl("./sounds/" .. soundFile .. '_' .. randomnum, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume)
     end)
 
     RegisterNetEvent('InteractSound_CL:PlayOnAll')
     AddEventHandler('InteractSound_CL:PlayOnAll', function(soundFile, soundVolume)
-		math.randomseed(GetGameTimer())
-		local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
+        math.randomseed(GetGameTimer())
+        local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
         PlayUrl("./sounds/" .. soundFile .. '_' .. randomnum, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume)
     end)
 
     RegisterNetEvent('InteractSound_CL:PlayWithinDistance')
     AddEventHandler('InteractSound_CL:PlayWithinDistance', function(playerNetId, maxDistance, soundFile, soundVolume)
-		math.randomseed(GetGameTimer())
-		local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
-        if GetPlayerFromServerId(playerNetId) ~= -1 then
+        math.randomseed(GetGameTimer())
+        local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
+	local is_networked = NetworkIsPlayerActive(GetPlayerFromServerId(playerNetId))
+        if GetPlayerFromServerId(playerNetId) ~= -1 and is_networked then
             local eCoords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(playerNetId)))
             PlayUrlPos("./sounds/" .. soundFile .. '_' .. randomnum, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume, eCoords)
             Distance("./sounds/" .. soundFile, maxDistance)
