9c1ca09fe95522e6ff3c89db149f8e22f1fefaea
Mohammad amin Jafarian <amin.jafarian82@gmail.com>
2021-05-28 15:40:14 +0430

    fix play one sound more than one time at same time
    Now u can play one sound more than one time before that sound finish

--- client/emulator/interact_sound/client.lua
+++ client/emulator/interact_sound/client.lua
@@ -2,19 +2,25 @@ if config.interact_sound_enable then
 
     RegisterNetEvent('InteractSound_CL:PlayOnOne')
     AddEventHandler('InteractSound_CL:PlayOnOne', function(soundFile, soundVolume)
-        PlayUrl("./sounds/" .. soundFile, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume)
+		math.randomseed(GetGameTimer())
+		local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
+        PlayUrl("./sounds/" .. soundFile .. '_' .. randomnum, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume)
     end)
 
     RegisterNetEvent('InteractSound_CL:PlayOnAll')
     AddEventHandler('InteractSound_CL:PlayOnAll', function(soundFile, soundVolume)
-        PlayUrl("./sounds/" .. soundFile, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume)
+		math.randomseed(GetGameTimer())
+		local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
+        PlayUrl("./sounds/" .. soundFile .. '_' .. randomnum, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume)
     end)
 
     RegisterNetEvent('InteractSound_CL:PlayWithinDistance')
     AddEventHandler('InteractSound_CL:PlayWithinDistance', function(playerNetId, maxDistance, soundFile, soundVolume)
+		math.randomseed(GetGameTimer())
+		local randomnum = math.random(1,100000000000)..''..math.random(1,100000000000)..''..math.random(1,100000000000)
         if GetPlayerFromServerId(playerNetId) ~= -1 then
             local eCoords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(playerNetId)))
-            PlayUrlPos("./sounds/" .. soundFile, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume, eCoords)
+            PlayUrlPos("./sounds/" .. soundFile .. '_' .. randomnum, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume, eCoords)
             Distance("./sounds/" .. soundFile, maxDistance)
         end
     end)
