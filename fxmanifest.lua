92c436846ba62e9154e5e1512792da077a1d4697
ElArthuro90 <99047216+ElArthuro90@users.noreply.github.com>
2022-07-03 12:47:12 +0200

    Add REDM
    Make this resource useable in RedM

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,7 @@
 fx_version 'cerulean'
-games { 'gta5' }
+games { 'gta5', 'rdr3' }
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_scripts {
 	"config.lua",
