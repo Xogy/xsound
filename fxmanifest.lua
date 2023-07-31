08109147e91306a809a1d98201da9f05f934b9e7
Marcell Nimmrichter <m.nimmrichter@profi-ag.de>
2023-07-31 15:15:41 +0200

    added name and version in fxmanifest to better import that source in other projects like "wasabi_boombox-2.1.5"

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,10 @@ games { 'gta5', 'rdr3' }
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+--[[ Resource Information ]]--
+name         'xsound'
+version      '1.4.3'
+
 client_scripts {
 	"config.lua",
 	"client/main.lua",
